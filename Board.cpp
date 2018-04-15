c5fccff741c650f5335a1ffb0f8269d5a1beaea0
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-04-15 17:24:25 -0600

    Create Board

---
+++ Board.cpp
@@ -0,0 +1,44 @@
+#include "Board.hpp"
+
+Board::Board() {
+  setRows(5);
+  setColumns(5);
+  gameBoard = new int*[rows];
+
+  for (int i = 0; i < rows; i++) {
+    gameBoard[i] = new int[columns];
+  }
+}
+
+Board::Board(int rowsToAdd, int colsToAdd) {
+  setRows(rowsToAdd);
+  setColumns(colsToAdd);
+  gameBoard = new int*[rows];
+
+  for (int i = 0; i < rows; i++) {
+    gameBoard[i] = new int[columns];
+  }
+}
+
+void Board::setRows(int rowsToAdd) {
+  rows = rowsToAdd;
+}
+
+int Board::getRows() {
+  return rows;
+}
+
+void Board::setColumns(int colsToAdd) {
+  columns = colsToAdd;
+}
+
+int Board::getColumns() {
+  return columns;
+}
+
+Board::~Board() {
+  for (int i = 0; i < rows; i++) {
+    delete [] gameBoard[i];
+  }
+  delete [] gameBoard;
+}
---
+++ Board.hpp
@@ -0,0 +1,21 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+class Board {
+  
+  private:
+    int** gameBoard;
+    int rows;
+    int columns;
+
+  public:
+    Board();
+    Board(int rowsToAdd, int colsToAdd);
+    void setRows(int rowsToAdd);
+    int getRows();
+    void setColumns(int colsToAdd);
+    int getColumns();
+    ~Board();
+};
+
+#endif
