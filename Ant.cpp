88aef7ba38e263dd2c3a361cf837c93ba89cf8ab
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-04-16 14:06:39 -0600

    Begin Ant specification

--- Ant.cpp
+++ Ant.cpp
@@ -1,1 +1,21 @@
 #include "Ant.hpp"
+
+
+Ant::Ant() {
+
+}
+
+
+Ant::Ant(int startingX, int startingY) {
+
+}
+
+
+void Ant::setOrientation(char direction) {
+
+}
+
+
+char Ant::getOrientation() {
+
+}
--- Ant.hpp
+++ Ant.hpp
@@ -1,12 +1,21 @@
 #ifndef ANT_HPP
 #define ANT_HPP
 
+
+#include "Board.hpp"
+
+
 class Ant {
 
   private:
+    Board antBoard;
     char orientation;
 
   public:
+    Ant();
+    Ant(int startingX, int startingY);
+    void setOrientation(char direction);
+    char getOrientation();
 
 };
 
