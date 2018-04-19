796f7d18e8abcdea63317066785bd67d4f66ec3a
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2018-04-18 19:38:09 -0600

    Added comments

--- Ant.cpp
+++ Ant.cpp
@@ -1,6 +1,10 @@
 #include "Ant.hpp"
 
 
+/***********************************************************************************************
+** Description: Initialize data members if no arguments are provided when an Ant object is
+** created.
+***********************************************************************************************/
 Ant::Ant() {
   setOrientation(N);
   setXCoord(0);
@@ -9,6 +13,10 @@ Ant::Ant() {
 }
 
 
+/***********************************************************************************************
+** Description: Initialize data members if arguments for a starting location and number of moves
+** are provided when an Ant object is created.
+***********************************************************************************************/
 Ant::Ant(int startingX, int startingY, int startingMoves) {
   setOrientation(N);
   setXCoord(startingX);
@@ -17,6 +25,11 @@ Ant::Ant(int startingX, int startingY, int startingMoves) {
 }
 
 
+/***********************************************************************************************
+** Description: Sets the orientation to left based on the current orientation of the Ant.
+** Then, either the current row or column is updated based on what would be considered a forward
+** movement from the updated orientation. Then, the number of moves is decremented by 1.
+***********************************************************************************************/
 void Ant::moveLeft() {
 
   switch (getOrientation()) {
@@ -54,6 +67,11 @@ void Ant::moveLeft() {
 }
 
 
+/***********************************************************************************************
+** Description: Sets the orientation to right based on the current orientation of the Ant.
+** Then, either the current row or column is updated based on what would be considered a forward
+** movement from the updated orientation. Then, the number of moves is decremented by 1.
+***********************************************************************************************/
 void Ant::moveRight() {
 
   switch (getOrientation()) {
@@ -91,41 +109,69 @@ void Ant::moveRight() {
 }
 
 
+/***********************************************************************************************
+** Description: Takes an integer value and sets the xCoord data member to this value to indicate
+** the Ant's current row.
+***********************************************************************************************/
 void Ant::setXCoord(int xIn) {
   xCoord = xIn;
 }
 
 
+/***********************************************************************************************
+** Description: Returns the xCoord data member as an integer when called.
+***********************************************************************************************/
 int Ant::getXCoord() {
   return xCoord;
 }
 
 
+/***********************************************************************************************
+** Description: Takes an integer value and sets the yCoord data member to this value to indicate
+** the Ant's current column.
+***********************************************************************************************/
 void Ant::setYCoord(int yIn) {
   yCoord = yIn;
 }
 
 
+/***********************************************************************************************
+** Description: Returns the yCoord data member as an integer when called.
+***********************************************************************************************/
 int Ant::getYCoord() {
  return yCoord;
 }
 
 
+/***********************************************************************************************
+** Description: Takes an enum data type representing the direction the Ant is facing and sets
+** the orientation data member to this value.
+***********************************************************************************************/
 void Ant::setOrientation(Direction dir) {
   orientation = dir;
 }
 
 
+/***********************************************************************************************
+** Description: Returns the orientation data member as a Direction enum data type when called.
+***********************************************************************************************/
 Direction Ant::getOrientation() {
   return orientation;
 }
 
 
+/***********************************************************************************************
+** Description: Takes an integer value representing the number of moves the Ant can make and
+** sets the moves data member to this value.
+***********************************************************************************************/
 void Ant::setMoves(int movesInput) {
   moves = movesInput;
 }
 
 
+/***********************************************************************************************
+** Description: Returns the moves data member as an integer when called.
+***********************************************************************************************/
 int Ant::getMoves() {
   return moves;
 }
--- Menu.cpp
+++ Menu.cpp
@@ -4,12 +4,24 @@
 
 #include "Menu.hpp"
 
+
+/***********************************************************************************************
+** Description: Prints a list of options that the user can be prompted to select to the screen.
+** Used before the getIntChoiceFromPrompt function so input can be retrieved.
+***********************************************************************************************/
 void displayMenu() {
   std::cout << "1) Start Langton’s Ant simulation" << std::endl;
   std::cout << "2) Quit" << std::endl;
 }
 
 
+/***********************************************************************************************
+** Description: Takes a references to a string used to indicate a prompt for the user to enter
+** data, followed by references to integers that output valid values to hint that the user
+** should enter an integer value within the entered values. Input is passed to checkIntInput
+** for basic validation, then the integer value the user entered is returned if it was valid.
+** Otherwise, the prompt is repeated.
+***********************************************************************************************/
 int getIntChoiceFromPrompt(const std::string &prompt, const int &minVal, const int &maxVal) {
 
   int userInput;
@@ -24,6 +36,15 @@ return userInput;
 
 }
 
+
+/***********************************************************************************************
+** Description: Takes a reference to an integer value to check, and minimum and maximum values.
+** If the input is of the incorrect type, this clears the failbit, ignores the rest of the
+** input, and returns false. If the input is an integer and in the valid range, the function
+** returns true. Otherwise, it returns false.
+** Source: http://en.cppreference.com/w/cpp/io/basic_ios/clear
+** Source: http://www.cplusplus.com/reference/istream/istream/ignore/
+***********************************************************************************************/
 bool checkIntInput(const int &inputVal, const int &minVal, const int &maxVal) {
 
   if (!std::cin) {
